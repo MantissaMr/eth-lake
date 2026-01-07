0f510c747d34e2ca95d55b26b2670c2cfe5b075c
Alameen Ibrahim <alamiin.sanusi@gmail.com>
2026-01-07 23:04:31 +0100

    feat: Implement async sleep function

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
 use pyo3::prelude::*;
+use pyo3_asyncio::tokio::future_into_py;
+use tokio::time;
 
-/// A Python module implemented in Rust.
-#[pymodule]
-mod eth_lake {
-    use pyo3::prelude::*;
-
-    /// Formats the sum of two numbers as string.
-    #[pyfunction]
+/// Sums two numbers and returns the result as a string
+#[pyfunction]
     fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
         Ok((a + b).to_string())
     }
+
+/// Asynchronously sleeps for a given number of seconds
+#[pyfunction]
+fn sleep_for (py: Python<'_>, seconds: u64) -> PyResult<&PyAny> {
+    future_into_py(py, async move {
+        time::sleep(std::time::Duration::from_secs(seconds)).await;
+        Ok(format!("Slept for {} seconds", seconds))
+    })
+}
+
+
+#[pymodule]
+fn eth_lake (_py: Python, m: &PyModule) -> PyResult<()>  { 
+    m.add_function(wrap_pyfunction!(sleep_for, m)?)?;
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+
+    Ok(())
 }
